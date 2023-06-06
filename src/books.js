4948811edfb38a47a3dcd2d4cca59aba44365b09
Hacker-KM <kartik.mishra3603@gmail.com>
2023-06-07 02:56:10 +0530

    first commit

---
+++ src/books.js
@@ -0,0 +1,23 @@
+import book1 from "./images/book1.jpg"
+import book2 from "./images/book2.jpg"
+import book3 from "./images/book3.jpg"
+export const books = [
+  {
+    author: "Jordan Moore",
+    title: "The Puppeteers",
+    img: book1,
+    id: 1,
+  },
+  {
+    author: "Emily Henry",
+    title: "Happy Place",
+    img: book2,
+    id: 2,
+  },
+  {
+    author: "Dr. Seuss",
+    title: "Oh, the Places You'll Go!  ",
+    img: book3,
+    id: 3,
+  },
+];
